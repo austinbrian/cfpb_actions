9031723a6cb32cbc018ea25176e7625b5baf9c47
Brian Austin <austin.brian@gmail.com>
2017-05-20 10:35:36 -0400

    added data to repo

---
+++ get_data.py
@@ -0,0 +1,28 @@
+# useful library for reading in data
+import pandas as pd
+
+# the base API
+api = "https://data.consumerfinance.gov/resource/jhzv-w97w.json"
+
+# the number of records we're going to request
+query = '?&$limit=100000'
+
+# additional API specifications I'm not using right now
+#$where=date%20between%20%272014-01-01T00:00:00%27%20and%20%272015-01-01T00:00:00%27'
+    
+# I have an app token in case we need that, but I haven't so far
+dataset_identifier = 'jhzv-w97w'
+APP_TOKEN = '48ozcpj4nCO3mqgJOl8GoIJgF'
+token = '?$$app_token='
+
+# total query we're running at the moment
+full_query = api+query
+
+# calls down the data from the CFPB API and reads it into memory
+# remember that we only requested 100,000 records (total is ~750k)
+cfpb = pd.read_json(full_query)
+
+# saves the data as a csv
+cfpb.to_csv('cfpb_sample_data.csv',encoding='utf-8',index=False)
+
+# this squeezes in under the 50M limit at which github yells at you
