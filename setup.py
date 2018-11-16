fe31891a0b9057670be1db2b6f02de717a5c2094
Brian Austin <austin.brian@gmail.com>
2018-11-15 20:25:30 -0500

    reorganized notebooks and models folders

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.2',
+    description='CFPB Actions',
+    author='Brian Austin',
+    license='MIT',
+)
