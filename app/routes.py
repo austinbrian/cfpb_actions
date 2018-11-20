d13a31d4f6bdb525bb079021981fbad7237d93c1
Brian Austin <austin.brian@gmail.com>
2018-11-20 18:56:49 -0500

    added initialization of flask routes

---
+++ app/routes.py
@@ -0,0 +1,10 @@
+import io
+from flask import render_template, flash, redirect, url_for, make_response, send_file, request
+from app import app, db
+from flask_login import current_user, login_user, logout_user, login_required
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html', 
+    title="Home") 
